8d5b9c76c1bd75c5957ab0d7c07741871b8e02c6
Eduarda Scharnhorst <eduardascharnhorst@gmail.com>
2020-12-30 00:32:00 -0300

    Add title to page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
-import HomePage from 'src/containers/Home/Home';
+import React from 'react';
+import Head from 'next/head';
+
+import Home from 'src/containers/Home/Home';
+
+function HomePage() {
+  return(
+    <>
+      <Head>
+        <title>Search | Rick and Morty</title>
+      </Head>
+      <Home />
+    </>
+  )
+}
 
 export default HomePage;
--- pages/list/index.js
+++ pages/list/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Head from 'next/head';
 
 import List from 'src/containers/List/List';
 
 function ListPage({ data, query }) {
   return (
-    <List characters={data} query={query} />
+    <>
+      <Head>
+        <title>Searching for &quot;{query}&quot; | Rick and Morty</title>
+      </Head>
+      <List characters={data} query={query} />
+    </>
   );
 }
 
 export async function getServerSideProps({query}) {
   const data = await fetch(`https://rickandmortyapi.com/api/character/?name=${query.character}`)
-    .then(res => res.json())
+    .then(res => {
+      if (!res.ok) {
+        return res.statusText
+      }
+
+      return res.json()
+    })
     .then(data => data)
   
   return { props: { data, query: query.character } }
@@ -22,15 +34,18 @@ ListPage.defaultProps = {
 }
 
 ListPage.propTypes = {
-  data: PropTypes.shape({
-    info: PropTypes.shape({
-      count: PropTypes.number,
-      pages: PropTypes.number,
-      next: PropTypes.string,
-      prev: PropTypes.string
-    }).isRequired,
-    results: PropTypes.arrayOf(PropTypes.object).isRequired,
-  }),
+  data: PropTypes.oneOfType([
+    PropTypes.shape({
+      info: PropTypes.shape({
+        count: PropTypes.number,
+        pages: PropTypes.number,
+        next: PropTypes.string,
+        prev: PropTypes.string
+      }).isRequired,
+      results: PropTypes.arrayOf(PropTypes.object).isRequired,
+    }),
+    PropTypes.string,
+  ]),
   query: PropTypes.string,
 }
 
