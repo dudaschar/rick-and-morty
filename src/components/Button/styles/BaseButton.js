ff1a4c6322c5b2046b665889dcf7b836f85b3991
Eduarda Scharnhorst <eduardascharnhorst@gmail.com>
2020-12-30 00:33:04 -0300

    Clean up button

--- src/components/Button/styles/BaseButton.js
+++ src/components/Button/styles/BaseButton.js
@@ -1,17 +1,10 @@
-import styled, { css } from 'styled-components';
-import PropTypes from 'prop-types';
-
-const handleCircleButton = css`
-  width: 3.5rem;
-  height: 3.5rem;
-  border-radius: 50%;
-`;
+import styled from 'styled-components';
 
 export const BaseButton = styled.button`
   border: none;
-  background-color: ${({ theme, color }) => color === 'white' ? theme.colors.white : theme.colors.primary};
-  color: ${({ theme, color }) => color === 'white' ? theme.colors.primary : theme.colors.white};
-  font-size: 1.5rem;
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 1rem;
   padding: ${({ theme }) => `${theme.spacing.small} ${theme.spacing.medium}`};
   cursor: pointer;
   flex-basis: auto;
@@ -20,17 +13,6 @@ export const BaseButton = styled.button`
     color: ${({ theme }) => theme.colors.grayLight};
     cursor: not-allowed;
   }
-  ${({ variant }) => variant ===  'circle' && handleCircleButton}
 `;
-
-BaseButton.defaultProps = {
-  color: undefined,
-  variant: undefined,
-};
-
-BaseButton.propTypes = {
-  color: PropTypes.string,
-  variant: PropTypes.string,
-};
 
 BaseButton.displayName = 'BaseButton';
