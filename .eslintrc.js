eb38bcb1e0d5227eaa320752308e559608094287
Eduarda Scharnhorst <eduardascharnhorst@gmail.com>
2021-01-01 17:23:37 -0300

    Config test environment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-undef */
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        'jest': true,
     },
     "extends": [
         "eslint:recommended",
